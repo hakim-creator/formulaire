ea555ba8af1dc98f0af0c2e0d0f1b8b3ff7a006e
Hakim <mloa.hakim@gmail.com>
2021-06-22 14:19:54 +0200

    project finish

---
+++ contact.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css">
+		 <link rel="manifest" href="manifest.json">
+	
+
+            <!-- Fallback application metadata for legacy browsers -->
+            <meta name="application-name" content="Assistance">
+            <link rel="icon" sizes="16x16 32x32 48x48" href="icon.png">
+            <link rel="icon" sizes="512x512" href="icon2.png">
+
+	<title>Contact</title>
+
+	 <script type="application/ld+json">
+      {
+        "@context":"http",
+        "type": "LocalBusiness",
+        "adresse": {
+        "@type": "PostAddress",
+        "adressLocality": "Toulouse",
+        "adressRegion": "",
+        "streetAdresse": "49 Avenue du Comminges, 31270 Cugnaux"
+      },
+      "description": ""
+      "name": "",
+      "telephone":"05 61 72 75 40"
+
+
+      }
+
+
+    </script>
+</head>
+<body>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<div><img src="ent.jpg" class="ent" style="width: 50px; "></div>
+	  <div class="container-fluid">
+	    <a class="btn btn-outline" href="index.php">Lycée Henri Matisse</a>
+	    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	      <span class="navbar-toggler-icon"></span>
+	    </button>
+
+	    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	      <ul class="navbar-nav ml-md-auto">
+	        <li class="nav-item">
+	          <a class="btn btn-outline-dark" aria-current="page" href="contact.php">Contact</a>
+	        </li>
+	        
+	      </ul>
+	      
+	    </div>
+	  </div>
+	</nav>
+	
+	<div class="container">
+     	<div class="row">
+
+	<footer class="contact">
+        <p class="lycee">Lycée Henri Matisse :</p>
+        <p class="adresse">Adresse : 49 Avenue du Comminges, 31270 Cugnaux</p>
+        <p class="telephone">Télephone: 05 61 72 75 40</p>
+        
+
+
+        <p>
+   	    <input class="btn btn-outline-dark" type="button" onclick="window.location.href = 'index.php';" value="Retour"/>
+      </p>
+
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3649.676714101722!2d1.3505406157746376!3d43.53105316833806!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12aeb7695cba5f29%3A0x7ae8481f807d3ce7!2sLyc%C3%A9e%20G%C3%A9n%C3%A9ral%20et%20Technologique%20Henri%20Matisse!5e1!3m2!1sfr!2sfr!4v1623660369840!5m2!1sfr!2sfr" width="200" height="200" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
+    </footer>
+
+     </div>
+    </div>
+
+      <footer class="fo-footer">
+
+
+      <div class="fo-footer__infos"></div>
+      <div><img src="ent.jpg" class="rounded float-start" style="width: 50px; " alt="mon-ent-occitanie"></div>
+      <a href="https://henri-matisse.mon-ent-occitanie.fr/" target="_blank">
+      <h5 class="fo-footer_nom">Lycée générale et téchnologique Henri Matisse</h5>
+      </a>
+      <a href="https://www.laregion.fr/" target="_blank">
+        <div><img src="occitanie.png" class="rounded float-end" style="width: 100px; margin-right: 13px; margin-top: -20px;" alt="la region occitanie"></div>
+      </a>
+      
+      
+    </footer>
+    
+
+    
+</body>
+</html>
